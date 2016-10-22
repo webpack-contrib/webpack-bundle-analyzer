de272fc662b20c82b7d42a0ffcbeba38ac211f98
th0r <grunin.ya@ya.ru>
2016-10-22 10:20:40 +0300

    Improve CLI tool

--- src/bin/analyzer.js
+++ src/bin/analyzer.js
@@ -1,11 +1,41 @@
 #! /usr/bin/env node
 
-'use strict';
+const path = require('path');
+
+const commander = require('commander');
+const { bold, magenta } = require('chalk');
 
 const analyzer = require('../analyzer');
 const viewer = require('../viewer');
 
-const [bundleStatsFile, bundleDir] = process.argv.slice(2);
+const program = commander
+  .version(require('../../package.json').version)
+  .usage(
+`<bundleStatsFile> [bundleDir] [options]
+
+  Arguments:
+  
+    bundleStatsFile  Path to Webpack Stats JSON file.
+                     It can be generated with ${bold('webpack --profile --json > stats.json')}
+    bundleDir        Directory containing all generated bundles. By default a directory of stats file is used.`
+  )
+  .option(
+    '-p, --port <n>',
+    'Port that will be used by bundle analyzer to start HTTP server.',
+    Number,
+    8888
+  )
+  .parse(process.argv);
+
+let {
+  port,
+  args: [bundleStatsFile, bundleDir]
+} = program;
+
+if (!bundleStatsFile) showHelp('Provide path to Webpack Stats file as first argument');
+if (isNaN(port)) showHelp('Invalid port number');
+
+if (!bundleDir) bundleDir = path.dirname(bundleStatsFile);
 
 let bundleStats;
 try {
@@ -15,4 +45,10 @@ try {
   process.exit(1);
 }
 
-viewer.start(bundleStats, { bundleDir });
+viewer.start(bundleStats, { bundleDir, port });
+
+function showHelp(error) {
+  if (error) console.log(`\n  ${magenta(error)}`);
+  program.outputHelp();
+  process.exit(1);
+}
--- src/parseUtils.js
+++ src/parseUtils.js
@@ -1,5 +1,3 @@
-'use strict';
-
 const fs = require('fs');
 const _ = require('lodash');
 const acorn = require('acorn');
