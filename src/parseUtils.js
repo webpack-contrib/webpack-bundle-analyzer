10eb9015686c5ffed38a952b48eaae9875e68b38
Vesa Laakso <laakso.vesa@gmail.com>
2017-02-04 16:40:42 +0200

    Support extra bundles with modules inside Array(N).concat([])

--- src/parseUtils.js
+++ src/parseUtils.js
@@ -39,6 +39,20 @@ function parseBundle(bundlePath) {
           return;
         }
 
+        // Additional bundle without webpack loader, with module IDs optimized.
+        // Modules are stored in second arguments Array(n).concat() call
+        // webpackJsonp([<chunks>], Array([minimum ID]).concat([<module>, <module>, ...]))
+        // As function name may be changed with `output.jsonpFunction` option we can't rely on it's default name.
+        if (
+          node.callee.type === 'Identifier' &&
+          args.length === 2 &&
+          isArgumentContainsChunkIds(args[0]) &&
+          isArgumentArrayConcatContainingChunks(args[1])
+        ) {
+          state.locations = getModulesLocationFromArrayConcat(args[1]);
+          return;
+        }
+
         // Main bundle with webpack loader
         // Modules are stored in first argument:
         // (function (...) {...})(<modules>)
@@ -96,6 +110,32 @@ function isArgumentContainsModulesList(arg) {
   return false;
 }
 
+function isArgumentArrayConcatContainingChunks(arg) {
+  if (
+    arg.type === 'CallExpression' &&
+    arg.callee.type === 'MemberExpression' &&
+    // Make sure the object called is `Array(<some number>)`
+    arg.callee.object.type === 'CallExpression' &&
+    arg.callee.object.callee.type === 'Identifier' &&
+    arg.callee.object.callee.name === 'Array' &&
+    arg.callee.object.arguments.length === 1 &&
+    isNumericId(arg.callee.object.arguments[0]) &&
+    // Make sure the property X called for `Array(<some number>).X` is `concat`
+    arg.callee.property.type === 'Identifier' &&
+    arg.callee.property.name === 'concat' &&
+    // Make sure exactly one array is passed in to `concat`
+    arg.arguments.length === 1 &&
+    arg.arguments[0].type === 'ArrayExpression'
+  ) {
+    // Modules are contained in `Array(<minimum ID>).concat(` array:
+    // https://github.com/webpack/webpack/blob/v1.14.0/lib/Template.js#L91
+    // The `<minimum ID>` + array indexes are module ids
+    return true;
+  }
+
+  return false;
+}
+
 function isModuleWrapper(node) {
   return (
     // It's an anonymous function expression that wraps module
@@ -137,6 +177,22 @@ function getModulesLocationFromFunctionArgument(arg) {
   }
 
   return {};
+}
+
+function getModulesLocationFromArrayConcat(arg) {
+  // arg(CallExpression) =
+  //   Array([minId]).concat([<minId module>, <minId+1 module>, ...])
+  //
+  // Get the [minId] value from the Array() call first argument literal value
+  const minId = arg.callee.object.arguments[0].value;
+  // The modules reside in the `concat()` function call arguments
+  const modulesNodes = arg.arguments[0].elements;
+
+  return _.transform(modulesNodes, (result, moduleNode, i) => {
+    if (!moduleNode) return;
+
+    result[i + minId] = getModuleLocation(moduleNode);
+  }, {});
 }
 
 function getModuleLocation(node) {
