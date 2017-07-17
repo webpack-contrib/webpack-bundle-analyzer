33fb8b286587b582a6bf8ec277a4e161495c6270
Vesa Laakso <laakso.vesa@gmail.com>
2017-07-17 16:09:02 +0300

    Convert webpack config tests to integration tests with Jest

--- integration_tests/webpack-configs/webpack-configs.test.js
+++ integration_tests/webpack-configs/webpack-configs.test.js
@@ -1,22 +1,23 @@
-const fs = require('fs');
+/* eslint-env jest */
+
+// const fs = require('fs');
 const del = require('del');
+
+const {
+  makeWebpackConfig,
+  webpackCompile
+} = require('./utils');
 
 let nightmare;
 
 describe('Webpack config', function () {
-  let clock;
-
-  this.timeout(3000);
-
-  before(function () {
+  beforeEach(function () {
     const Nightmare = require('nightmare');
     nightmare = Nightmare();
     del.sync(`${__dirname}/output`);
-    clock = sinon.useFakeTimers();
   });
 
   beforeEach(async function () {
-    this.timeout(10000);
     await nightmare.goto('about:blank');
   });
 
@@ -24,18 +25,12 @@ describe('Webpack config', function () {
     del.sync(`${__dirname}/output`);
   });
 
-  after(function () {
-    clock.restore();
-  });
-
-  it('with query in bundle filename should be supported', async function () {
+  fit('with query in bundle filename should be supported', async function () {
     const config = makeWebpackConfig();
 
     config.output.filename = 'bundle.js?what=is-this-for';
 
     await webpackCompile(config);
-    clock.tick(1);
-
     await expectValidReport();
   });
 
@@ -47,8 +42,6 @@ describe('Webpack config', function () {
     config.output.jsonpFunction = 'somethingCompletelyDifferent';
 
     await webpackCompile(config);
-    clock.tick(1);
-
     await expectValidReport({
       parsedSize: 445,
       gzipSize: 179
@@ -64,20 +57,23 @@ describe('Webpack config', function () {
     ];
 
     await webpackCompile(config);
-    clock.tick(1);
-
     const chartData = await getChartDataFromReport();
-    expect(chartData[0].groups).to.containSubset([{
+    expect(chartData[0].groups).toContainEqual({
+      gzipSize: expect.anything(),
+      id: expect.anything(),
+      parsedSize: expect.anything(),
+      statSize: expect.anything(),
+      weight: expect.anything(),
       label: 'multi ./src/a.js ./src/b.js',
       path: './multi ./src/a.js ./src/b.js',
       groups: undefined
-    }]);
+    });
   });
 });
 
 async function expectValidReport(opts) {
   const {
-    bundleFilename = 'bundle.js',
+    // bundleFilename = 'bundle.js',
     reportFilename = 'report.html',
     bundleLabel = 'bundle.js',
     statSize = 141,
@@ -85,10 +81,10 @@ async function expectValidReport(opts) {
     gzipSize = 820
   } = opts || {};
 
-  expect(fs.existsSync(`${__dirname}/output/${bundleFilename}`)).to.be.true;
-  expect(fs.existsSync(`${__dirname}/output/${reportFilename}`)).to.be.true;
+  // expect(fs.existsSync(`${__dirname}/output/${bundleFilename}`)).toEqual(true);
+  // expect(fs.existsSync(`${__dirname}/output/${reportFilename}`)).toEqual(true);
   const chartData = await getChartDataFromReport(reportFilename);
-  expect(chartData[0]).to.containSubset({
+  expect(chartData[0]).toMatchObject({
     label: bundleLabel,
     statSize,
     parsedSize,
@@ -97,7 +93,9 @@ async function expectValidReport(opts) {
 }
 
 async function getChartDataFromReport(reportFilename = 'report.html') {
+  /* global window */
   return await nightmare
     .goto(`file://${__dirname}/output/${reportFilename}`)
-    .evaluate(() => window.chartData);
+    .evaluate(() => window.chartData)
+    .end();
 }
