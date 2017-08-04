272b68edb92113f9a55e10420a685b78f8cac7d0
th0r <grunin.ya@ya.ru>
2017-08-04 17:45:27 +0300

    Add chunk sizes to sidebar

--- client/components/CheckboxListItem.jsx
+++ client/components/CheckboxListItem.jsx
@@ -1,21 +1,37 @@
 /** @jsx h */
 import { h, Component } from 'preact';
 
+import CheckboxList from './CheckboxList';
+
 import s from './CheckboxList.css';
 
 export default class CheckboxListItem extends Component {
 
   render() {
-    const { item, checked } = this.props;
+    const { checked } = this.props;
 
     return (
       <label className={s.item}>
-        <input className={s.checkbox} type="checkbox" checked={checked}
+        <input className={s.checkbox}
+          type="checkbox"
+          checked={checked}
           onChange={this.handleChange}/>
-        <span className={s.itemText}>
-          {item.label}
-        </span>
+        {this.renderLabel()}
       </label>
+    );
+  }
+
+  renderLabel() {
+    const { children, item } = this.props;
+
+    if (children && children.length) {
+      return children[0](item, s.itemText);
+    }
+
+    return (
+      <span className={s.itemText}>
+        {item === CheckboxList.ALL_ITEM ? 'All' : item.label}
+      </span>
     );
   }
 
