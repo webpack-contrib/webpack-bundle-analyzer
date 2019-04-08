2b53d43694fed86d61bdd5981907c3144391c604
Ben Regenspan <bregenspan@gmail.com>
2019-04-08 13:10:38 -0400

    Document event type

--- client/components/Treemap.jsx
+++ client/components/Treemap.jsx
@@ -89,6 +89,12 @@ export default class Treemap extends Component {
           };
         }
       },
+      /**
+       * Handle Foamtree's "group clicked" event
+       * @param {FoamtreeEvent} event - Foamtree event object
+       *  (see https://get.carrotsearch.com/foamtree/demo/api/index.html#event-details)
+       * @returns {void}
+       */
       onGroupClick(event) {
         preventDefault(event);
         if ((event.ctrlKey || event.secondary) && props.onGroupSecondaryClick) {
