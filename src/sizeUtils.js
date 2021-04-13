4dfbb5ccf6623699dcb4ab3be87321e06f71ffaa
David Császár <dcsaszar@users.noreply.github.com>
2021-04-13 14:18:29 +0200

    Add Brotli size helper

--- src/sizeUtils.js
+++ src/sizeUtils.js
@@ -3,3 +3,11 @@ const zlib = require('zlib');
 export function gzipSize(input) {
   return zlib.gzipSync(input, {level: 9}).length;
 }
+
+export function brotliSize(input) {
+  if (typeof zlib.brotliCompressSync !== 'function') {
+    throw new Error('Brotli compression requires Node.js v10.16.0 or higher.');
+  }
+
+  return zlib.brotliCompressSync(input).length;
+}
