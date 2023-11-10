5fa40f53706c120f32bb546f82178f572376d5d9
Chris Krogh <christopher.krogh@faire.com>
2023-11-10 09:57:01 -0500

    filter entrypoints

--- client/components/Dropdown.jsx
+++ client/components/Dropdown.jsx
@@ -1,25 +1,88 @@
+import {createRef} from 'preact';
 import PureComponent from '../lib/PureComponent';
 
 import s from './Dropdown.css';
+
 export default class Dropdown extends PureComponent {
+  input = createRef();
+
+  state = {
+    query: '',
+    showOptions: false
+  };
+
+  componentDidMount() {
+    document.addEventListener('click', this.handleClickOutside, true);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside, true);
+  }
 
   render() {
-    const {label, defaultOption, onSelectionChange, options} = this.props;
+    const {label, options} = this.props;
+
+    const filteredOptions =
+      this.state.query
+        ? options.filter((option) =>
+          option.toLowerCase().includes(this.state.query.toLowerCase())
+        )
+        : options;
 
     return (
       <div className={s.container}>
-        <div className={s.label}>
-          {label}:
-        </div>
+        <div className={s.label}>{label}:</div>
         <div>
-          <select className={s.select} id={label} name={label} onChange={onSelectionChange}>
-            <option value={defaultOption}>{defaultOption}</option>
-            {options.map(option =>
-              <option key={option} value={option}>{option}</option>
-            )}
-          </select>
+          <input ref={this.input}
+            className={s.input}
+            type="text"
+            value={this.state.query}
+            onInput={this.handleInput}
+            onFocus={this.handleFocus}/>
+          {this.state.showOptions ? (
+            <div className={s.options}>
+              {filteredOptions.map((option) => (
+                <div key={option}
+                  className={s.option}
+                  onClick={this.getOptionClickHandler(option)}>
+                  {option}
+                </div>
+              ))}
+            </div>
+          ) : null}
         </div>
       </div>
     );
   }
+
+  handleClickOutside = (event) => {
+    const el = this.input.current;
+    if (el && event && !el.contains(event.target)) {
+      this.setState({showOptions: false});
+      // If the query is not in the options, reset the selection
+      if (this.state.query && !this.props.options.some((option) => option === this.state.query)) {
+        this.setState({query: ''});
+        this.props.onSelectionChange(undefined);
+      }
+    }
+  };
+
+  handleInput = (event) => {
+    const {value} = event.target;
+    this.setState({query: value});
+    if (!value) {
+      this.props.onSelectionChange(undefined);
+    }
+  }
+
+  handleFocus = () => {
+    // move the cursor to the end of the input
+    this.input.current.value = this.state.query;
+    this.setState({showOptions: true});
+  }
+
+  getOptionClickHandler = (option) => () => {
+    this.props.onSelectionChange(option);
+    this.setState({query: option, showOptions: false});
+  };
 }
