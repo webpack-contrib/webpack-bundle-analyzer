c68dfb7129e16e4cb70a26f8140db6e857c462be
Arnav <info@arnavgupta.net>
2024-04-13 21:00:59 +0530

    fix: reverted format changes on analyzer.js

--- src/bin/analyzer.js
+++ src/bin/analyzer.js
@@ -1,22 +1,21 @@
 #! /usr/bin/env node
 
-const { resolve, dirname } = require("path");
+const {resolve, dirname} = require('path');
 
-const commander = require("commander");
-const { magenta } = require("picocolors");
+const commander = require('commander');
+const {magenta} = require('picocolors');
 
-const analyzer = require("../analyzer");
-const viewer = require("../viewer");
-const Logger = require("../Logger");
-const utils = require("../utils");
+const analyzer = require('../analyzer');
+const viewer = require('../viewer');
+const Logger = require('../Logger');
+const utils = require('../utils');
 
-const options = require("./bundle-analyzer-flags.js");
-const { cli } = require("webpack");
+const SIZES = new Set(['stat', 'parsed', 'gzip']);
 
-const SIZES = new Set(["stat", "parsed", "gzip"]);
-
-const program = commander.version(require("../../package.json").version).usage(
-  `<bundleStatsFile> [bundleDir] [options]
+const program = commander
+  .version(require('../../package.json').version)
+  .usage(
+`<bundleStatsFile> [bundleDir] [options]
 
   Arguments:
 
@@ -24,45 +23,58 @@ const program = commander.version(require("../../package.json").version).usage(
     bundleDir        Directory containing all generated bundles.
                      You should provided it if you want analyzer to show you the real parsed module sizes.
                      By default a directory of stats file is used.`
-);
-
-const logger = new Logger(logLevel);
-
-Object.entries(options).forEach(([key, value]) => {
-  const optionName = key.replace(/([A-Z])/g, "-$1").toLowerCase();
-  const defaultValue = value.configs[0].defaultValue;
-  const description = value.description;
-
-  switch (value.configs[0].type) {
-    case "boolean":
-      program.option(
-        `-${optionName.charAt(0)}, --${optionName}`,
-        description,
-        defaultValue
-      );
-      break;
-    case "enum":
-      const values = value.configs[0].values.join("|");
-      program.option(
-        `-${optionName.charAt(0)}, --${optionName} <${values}>`,
-        description,
-        defaultValue
-      );
-      break;
-    case "string":
-    case "number":
-      program.option(
-        `-${optionName.charAt(0)}, --${optionName} <value>`,
-        description,
-        defaultValue.toString()
-      );
-      break;
-    default:
-      logger.warn(`Unknown type: ${value.configs[0].type}`);
-  }
-});
-
-program.parse(process.argv);
+  )
+  .option(
+    '-m, --mode <mode>',
+    'Analyzer mode. Should be `server`,`static` or `json`.' +
+      br('In `server` mode analyzer will start HTTP server to show bundle report.') +
+      br('In `static` mode single HTML file with bundle report will be generated.') +
+      br('In `json` mode single JSON file with bundle report will be generated.'),
+    'server'
+  )
+  .option(
+    // Had to make `host` parameter optional in order to let `-h` flag output help message
+    // Fixes https://github.com/webpack-contrib/webpack-bundle-analyzer/issues/239
+    '-h, --host [host]',
+    'Host that will be used in `server` mode to start HTTP server.',
+    '127.0.0.1'
+  )
+  .option(
+    '-p, --port <n>',
+    'Port that will be used in `server` mode to start HTTP server.',
+    8888
+  )
+  .option(
+    '-r, --report <file>',
+    'Path to bundle report file that will be generated in `static` mode.'
+  )
+  .option(
+    '-t, --title <title>',
+    'String to use in title element of html report.'
+  )
+  .option(
+    '-s, --default-sizes <type>',
+    'Module sizes to show in treemap by default.' +
+    br(`Possible values: ${[...SIZES].join(', ')}`),
+    'parsed'
+  )
+  .option(
+    '-O, --no-open',
+    "Don't open report in default browser automatically."
+  )
+  .option(
+    '-e, --exclude <regexp>',
+    'Assets that should be excluded from the report.' +
+    br('Can be specified multiple times.'),
+    array()
+  )
+  .option(
+    '-l, --log-level <level>',
+    'Log level.' +
+    br(`Possible values: ${[...Logger.levels].join(', ')}`),
+    Logger.defaultLevel
+  )
+  .parse(process.argv);
 
 let [bundleStatsFile, bundleDir] = program.args;
 let {
@@ -74,43 +86,25 @@ let {
   defaultSizes,
   logLevel,
   open: openBrowser,
-  exclude: excludeAssets,
+  exclude: excludeAssets
 } = program.opts();
+const logger = new Logger(logLevel);
 
-try {
-  cli.processArguments(options, process.argv);
-} catch (problem) {
-  logger.error(`An error occurred processing arguments: ${problem}`);
-  process.exit(1);
-}
-
-Object.entries(options).forEach(([key, optionConfig]) => {
-  optionConfig.configs.forEach((config) => {
-    cli.option(config.path, config.description, config.defaultValue);
-  });
-});
-
-if (typeof reportTitle === "undefined") {
+if (typeof reportTitle === 'undefined') {
   reportTitle = utils.defaultTitle;
 }
 
-if (!bundleStatsFile)
-  showHelp("Provide path to Webpack Stats file as first argument");
-if (mode !== "server" && mode !== "static" && mode !== "json") {
-  showHelp("Invalid mode. Should be either `server`, `static` or `json`.");
+if (!bundleStatsFile) showHelp('Provide path to Webpack Stats file as first argument');
+if (mode !== 'server' && mode !== 'static' && mode !== 'json') {
+  showHelp('Invalid mode. Should be either `server`, `static` or `json`.');
 }
-if (mode === "server") {
-  if (!host) showHelp("Invalid host name");
+if (mode === 'server') {
+  if (!host) showHelp('Invalid host name');
 
-  port = port === "auto" ? 0 : Number(port);
-  if (isNaN(port)) showHelp("Invalid port. Should be a number or `auto`");
+  port = port === 'auto' ? 0 : Number(port);
+  if (isNaN(port)) showHelp('Invalid port. Should be a number or `auto`');
 }
-if (!SIZES.has(defaultSizes))
-  showHelp(
-    `Invalid default sizes option. Possible values are: ${[...SIZES].join(
-      ", "
-    )}`
-  );
+if (!SIZES.has(defaultSizes)) showHelp(`Invalid default sizes option. Possible values are: ${[...SIZES].join(', ')}`);
 
 bundleStatsFile = resolve(bundleStatsFile);
 
@@ -121,7 +115,7 @@ parseAndAnalyse(bundleStatsFile);
 async function parseAndAnalyse(bundleStatsFile) {
   try {
     const bundleStats = await analyzer.readStatsFromFile(bundleStatsFile);
-    if (mode === "server") {
+    if (mode === 'server') {
       viewer.startServer(bundleStats, {
         openBrowser,
         port,
@@ -131,30 +125,28 @@ async function parseAndAnalyse(bundleStatsFile) {
         bundleDir,
         excludeAssets,
         logger: new Logger(logLevel),
-        analyzerUrl: utils.defaultAnalyzerUrl,
+        analyzerUrl: utils.defaultAnalyzerUrl
       });
-    } else if (mode === "static") {
+    } else if (mode === 'static') {
       viewer.generateReport(bundleStats, {
         openBrowser,
-        reportFilename: resolve(reportFilename || "report.html"),
+        reportFilename: resolve(reportFilename || 'report.html'),
         reportTitle,
         defaultSizes,
         bundleDir,
         excludeAssets,
-        logger: new Logger(logLevel),
+        logger: new Logger(logLevel)
       });
-    } else if (mode === "json") {
+    } else if (mode === 'json') {
       viewer.generateJSONReport(bundleStats, {
-        reportFilename: resolve(reportFilename || "report.json"),
+        reportFilename: resolve(reportFilename || 'report.json'),
         bundleDir,
         excludeAssets,
-        logger: new Logger(logLevel),
+        logger: new Logger(logLevel)
       });
     }
   } catch (err) {
-    logger.error(
-      `Couldn't read webpack bundle stats from "${bundleStatsFile}":\n${err}`
-    );
+    logger.error(`Couldn't read webpack bundle stats from "${bundleStatsFile}":\n${err}`);
     logger.debug(err.stack);
     process.exit(1);
   }
@@ -167,7 +159,7 @@ function showHelp(error) {
 }
 
 function br(str) {
-  return `\n${" ".repeat(28)}${str}`;
+  return `\n${' '.repeat(28)}${str}`;
 }
 
 function array() {
